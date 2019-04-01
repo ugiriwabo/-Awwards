04631beba8b1ac926870be6706fb64287aa21c64
ugiriwabo <ugiraconsole@gmail.com>
2019-04-01 14:26:23 +0200

    add a form of search

--- awwrds/settings.py
+++ awwrds/settings.py
@@ -77,10 +77,19 @@ WSGI_APPLICATION = 'awwrds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awward',
+        'USER': 'wecode',
+    'PASSWORD':'0789090427',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -107,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- points/models.py
+++ points/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+# from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(models.Model):
+    image=models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =30)
+    caption = models.TextField(max_length =120)
+    post_date = models.DateTimeField(auto_now=True)
+    likes = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+        
+    def save_image(self):
+        self.save() 
+
+    @classmethod
+    def get_image(cls,id):
+        Image.objects.all()
+
+    
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    username=models.CharField(max_length =30)
+    profile_photo = models.ImageField(upload_to = 'pic/')
+    bio=models.CharField(max_length =30)
+
+
+    def save_profile(self):
+        self.save() 
+    
+    @classmethod
+    def get_profile(cls,id):
+        Profile.objects.all()
+
+    def delete_profile(self):
+       self.delete()
+
+    def update_bio(self,bio):
+         self.bio=bio
+         self.save()
+
+    @classmethod
+    def search_by_user(cls,search_term):
+        user=Profile.objects.filter(name__icontains=search_term)
+        users = cls.objects.filter(user=user)
+        return users
