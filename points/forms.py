07f6f7f4524064cb7aafa4f2e5696b86f90ad352
ugiriwabo <ugiraconsole@gmail.com>
2019-04-01 14:42:14 +0200

    add a file which colled forms.py and i inserted some code

---
+++ points/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Profile,Image,Comment
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        
--- points/urls.py
+++ points/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
@@ -7,3 +9,5 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     url(r'^search/', views.welcome, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
